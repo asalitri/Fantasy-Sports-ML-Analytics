170bc7bf2cc312e4c504cd96e0ef2490fbc247e1
asalitri <asalitri@purdue.edu>
2025-05-21 23:39:42 -0500

    finished get_league_ids() and updated all past season league ids

--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,7 @@
 from yahoo_oauth import OAuth2
 from yahoo_fantasy_api import Game
 
-def get_league_ids(start_year=2017, end_year=2025):
+def get_league_ids(start_year=2017, end_year=2026):
     oauth = OAuth2(None, None, from_file='oauth2.json')
     gm = Game(oauth, 'nfl')
 
@@ -11,10 +11,13 @@ def get_league_ids(start_year=2017, end_year=2025):
             if league_ids:
                 for league_id in league_ids:
                     league = gm.to_league(league_id)
-                    setting = league.settings()
+                    settings = league.settings()
                     name = settings.get('name')
                     print(f"{year} - {name}: {league_id}")
             else:
                 print(f"{year}: No leagues found.")
         except Exception as e:
             print(f"{year}: Error — {e}")
+
+if __name__ == "__main__":
+    get_league_ids()
